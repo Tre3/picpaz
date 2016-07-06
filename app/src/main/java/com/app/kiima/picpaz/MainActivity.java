b9c58ead3ebf97e878d74ee415ec3caab7d4f1af
Tre3 <keitree33@gmail.com>
2016-07-07 00:27:03 +0900

    Push通知周りの処理を追加

--- app/src/main/java/com/app/kiima/picpaz/MainActivity.java
+++ app/src/main/java/com/app/kiima/picpaz/MainActivity.java
@@ -1,13 +1,21 @@
 package com.app.kiima.picpaz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+/**
+ * Created by Tre3 on 2016/07/06.
+ */
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAnalytics mFirebaseAnalytics;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                String value = getIntent().getExtras().getString(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+
+        Log.d(TAG, "InstanceID token: " + FirebaseInstanceId.getInstance().getToken());
+
+        Intent serviceIntent = new Intent(this, MyFirebaseMessagingService.class);
+        startService(serviceIntent);
     }
 }
