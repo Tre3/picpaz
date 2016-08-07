61a45146b0c2102ed4ae1dfd9895685be434252b
Tre3 <keitree33@gmail.com>
2016-08-07 15:39:20 +0900

    パズル用のビューのインターフェースを定義

---
+++ app/src/main/java/com/app/kiima/picpaz/view/view_interfaces/PuzzleFrame.java
@@ -0,0 +1,14 @@
+package com.app.kiima.picpaz.view.view_interfaces;
+
+import java.util.List;
+
+/**
+ * Created by Tre3 on 2016/08/07.
+ */
+
+public abstract class PuzzleFrame {
+
+    public PuzzleFrame(List<PuzzlePieceInterface> pieces) {
+
+    }
+}
---
+++ app/src/main/java/com/app/kiima/picpaz/view/view_interfaces/PuzzlePieceInterface.java
@@ -0,0 +1,8 @@
+package com.app.kiima.picpaz.view.view_interfaces;
+
+/**
+ * Created by Tre3 on 2016/08/07.
+ */
+
+public class PuzzlePieceInterface {
+}
